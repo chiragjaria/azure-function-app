cc606131ded057f09a6fab76607cc97af90646f5
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-05 12:52:45 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,18 @@
+variable "resource_group_name" {}
 
+variable "location" {
+}
+
+variable "storage_account_name" {
+}
+
+variable "function_app_name" {
+}
+
+variable "key_vault_name" {
+}
+
+variable "db_host" {}
+variable "db_name" {}
+variable "db_user" {}
+variable "db_pass" {}
