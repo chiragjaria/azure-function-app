0a5c23303183cf9c9824c9c58c2f6569f6cfc59e
chiragjaria <65189093+chiragjaria@users.noreply.github.com>
2026-04-05 13:10:08 +0530

    updated output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
+output "function_app_name" {
+  value = azurerm_linux_function_app.func.name
+}
 
+output "function_app_url" {
+  value = "https://${azurerm_linux_function_app.func.default_hostname}"
+}
+
+output "function_identity_principal_id" {
+  value = azurerm_linux_function_app.func.identity[0].principal_id
+}
